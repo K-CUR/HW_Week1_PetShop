bd7b980a2ad0a2eb282de8a6ba43ca6ca365621b
Katy Currie <katylcurrie@gmail.com>
2021-03-14 19:38:59 +0000

    Homework mvp & first 3 of optional completed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -4,26 +4,38 @@ def get_pet_shop_name(pet_shop):
     return pet_shop["name"]
 
 # 2
+
+
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
 
 # 3
+
+
 def add_or_remove_cash(pet_shop, amount):
     pet_shop["admin"]["total_cash"] += amount
 
 # 4
+
+
 def get_pets_sold(pet_shop):
     return pet_shop["admin"]["pets_sold"]
 
 # 5
+
+
 def increase_pets_sold(pet_shop, qty_sold):
     pet_shop["admin"]["pets_sold"] += qty_sold
 
 # 6
+
+
 def get_stock_count(pet_shop):
     return len(pet_shop["pets"])
 
 # 7
+
+
 def get_pets_by_breed(pet_shop, breed_name):
     selected_breed_list = []
     for breed in pet_shop["pets"]:
@@ -32,37 +44,58 @@ def get_pets_by_breed(pet_shop, breed_name):
     return selected_breed_list
 
 # 8
+
+
 def find_pet_by_name(pet_shop, pet_name):
     for name in pet_shop["pets"]:
         if name["name"] == pet_name:
             return(name)
 
 # 9
+
+
 def remove_pet_by_name(pet_shop, pet_name):
     for name in pet_shop["pets"]:
         if name["name"] == pet_name:
             pet_shop["pets"].remove(name)
 
 # 10
+
+
 def add_pet_to_stock(pet_shop, new_pet):
     pet_shop["pets"].append(new_pet)
 
 # 11
+
+
 def get_customer_cash(customers):
     return customers["cash"]
 
 # 12
+
+
 def remove_customer_cash(customer, cash_to_remove):
     customer["cash"] -= cash_to_remove
 
 # 13
+
+
 def get_customer_pet_count(customer):
     if customer["pets"] == []:
         return 0
     else:
         return len(customer["pets"])
 # 14
+
+
 def add_pet_to_customer(customers, add_pet):
     customers["pets"].append(add_pet)
 
 # END OF MVP
+
+def customer_can_afford_pet(customer, new_pet):
+    if customer["cash"] >= new_pet["price"]:
+        return(bool(customer_can_afford_pet))
+    else:
+        return bool(False)
+
